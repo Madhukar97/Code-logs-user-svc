5b1fb3f9e7ddd8ec04b0923fd6b4df489a73d4d7
Madhukar <madhukar.tatiraju@gmail.com>
2023-01-29 18:43:58 +0530

    initial commit

---
+++ src/main/java/com/codelogs/CodeLogsUserSvcApplication.java
@@ -0,0 +1,13 @@
+package com.codelogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLogsUserSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLogsUserSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelogs/CodeLogsUserSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLogsUserSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
